a60ea730bc3eed2f2840e747866eda9ca59ae0f5
Silicontent <61814046+Silicontent@users.noreply.github.com>
2022-10-13 00:24:29 -0500

    added stone and player character
    removed TempDirt, added programmer art stone and player (very well drawn)

---
+++ scripts/Player.gd
@@ -0,0 +1,98 @@
+# This movement code is from https://kidscancode.org/godot_recipes/2d/platform_character/
+
+extends KinematicBody2D
+
+const GRAV := 1000
+const ACC := 0.15
+const FRIC := 0.35
+
+var start_pos = Vector2(8, -68)
+var speed := 100
+var velocity := Vector2.ZERO
+
+var jump_speed := -325
+var vspeed := 0.0
+var grounded := false
+var is_jumping := false
+var coy_time:= false
+
+onready var sprite = $Sprite
+onready var coy_timer = $CoyoteTimer
+
+
+func _ready() -> void:
+	to_spawn()
+
+func to_spawn():
+	velocity = Vector2.ZERO
+	global_position = start_pos
+
+
+# MOVEMENT ---------------------------------------------------------------
+func movement():
+	var dir = 0
+	
+	if Input.is_action_pressed("mv_left"):
+		dir -= 1
+		sprite.flip_h = true
+	if Input.is_action_pressed("mv_right"):
+		dir += 1
+		sprite.flip_h = false
+	
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, ACC)
+	else:
+		velocity.x = lerp(velocity.x, 0, FRIC)
+
+
+func vert_movement(delta):
+	if(is_on_ceiling()):
+		velocity.y = 10
+		vspeed = 10
+	
+	if(grounded == true and !is_on_floor()): # just left the ground
+		coy_time = true
+		coy_timer.start()
+		
+	if(is_on_floor()):
+		grounded = true
+		coy_timer.stop()
+		coy_time = false
+	else:
+		grounded = false
+
+	if(!is_on_floor()):
+		vspeed += (GRAV * delta)
+	else:
+		vspeed = 0
+		if(Input.is_action_just_pressed("mv_jump")):
+			coy_time = false
+			coy_timer.stop()
+			vspeed = jump_speed
+			
+	if(coy_time and Input.is_action_just_pressed("mv_jump")):
+		coy_time = false
+		coy_timer.stop()
+		vspeed = jump_speed
+
+	velocity.y = vspeed
+
+
+func _physics_process(delta):
+	movement()
+	vert_movement(delta)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	# JUMPING
+	if Input.is_action_just_pressed("mv_jump"):
+		if grounded and is_jumping == false:
+			velocity.y = jump_speed
+			is_jumping = true
+			coy_timer.stop()
+	
+	if Input.is_action_just_pressed("util_enter"):
+		to_spawn()
+
+
+func _on_CoyoteTimer_timeout():
+	coy_time = false
--- scripts/WorldGen.gd
+++ scripts/WorldGen.gd
@@ -1,14 +1,10 @@
 extends TileMap
 
-export(int) var world_x = 10
-export(int) var world_y = 10
-export(int) var surf_y = 20
-export(int) var ug_y = 280
+export (int) var world_x = 10
+export (int) var world_y = 10
 
-var noise: OpenSimplexNoise = OpenSimplexNoise.new()
-
-var snap_x = 8
-var snap_y = 8
+var snap_x = 16
+var snap_y = 16
 
 
 # WORLD ELEVATION GENERATION ---------------------------------------------
@@ -17,97 +13,8 @@ func gen_base():
 		for y in world_y:
 			var tile_id = 0
 			set_cell(x, y, tile_id)
-	
-"""
-	for x in world_x:
-		for y in ug_y:
-			var tile_id = 1
-			set_cell(x, surf_y+y, tile_id)
+			update_bitmask_area(Vector2(x, y))
 
 
-func gen_splotches():
-	set_noise_prop(0, 5.0, 0.588, 2.43)
-	
-	for x in world_x:
-		for y in surf_y:
-			var tile_id = gen_splotch_id(noise.get_noise_2d(x, y), 0, 1)
-			set_cell(x, y, tile_id)
-	
-	for x in world_x:
-		for y in ug_y:
-			var tile_id = gen_splotch_id(noise.get_noise_2d(x, y), 1, 0)
-			set_cell(x, surf_y+y, tile_id)
-
-# function for getting ID of tile (used by gen_splotches)
-func gen_splotch_id(noise_level: float, splotch_tile: int, main_tile: int):
-	var tile_to_gen = splotch_tile
-	
-	if noise_level <= -0.2:
-		return main_tile
-	else:
-		return splotch_tile
-
-# originally "if noise.get_noise_2d(x, y) <= -0.2
-func gen_caves():
-	noise.seed = randi()
-	set_noise_prop(0, 5.0, 0.588, 2.43)
-	
-	for x in world_x:
-		for y in ug_y:
-			if noise.get_noise_2d(x, y) <= 0.1:
-				set_cell(x, surf_y+y, -1)
-
-
-func gen_surface():
-	set_noise_prop(1, 6.2, 0.486, 2.0)
-	
-	for x in world_x:
-		for y in surf_y-10:
-			if noise.get_noise_2d(x, y) <= -0.1:
-				set_cell(x, 10+y, -1)
-	
-	for x in world_x:
-		var current_tile = get_cell(x, 0)
-		if current_tile != -1:
-			set_cell(x, 0, 3)
-"""
-
-
-
-# WORLD FUNCTIONS --------------------------------------------------------
-func generate_level():
-	randomize()
-	noise.seed = randi()
-	
-	gen_base()
-#	gen_splotches()
-#	gen_caves()
-#	gen_surface()
-
-"""
-func set_noise_prop(oct: int, per: float, pers: float, lac: float):
-	noise.octaves = oct
-	noise.period = per
-	noise.persistence = pers
-	noise.lacunarity = lac
-
-
-# BREAKING / PLACING -----------------------------------------------------
-func _on_Player_mine(selector):
-	var tile: Vector2 = world_to_map(selector.mouse_pos * 8) # gets the tile we clicked on
-	var tile_id = get_cellv(tile) # returns the ID of that cell
-	
-	var new_id = -1
-	
-	if tile_id != -1: # we clicked on a mud block
-		new_id = -1
-	set_cellv(tile, new_id)
-
-func _on_Player_place(selector):
-	var tile: Vector2 = world_to_map(selector.mouse_pos * 8)
-	set_cellv(tile, 2)
-"""
-
-# OTHER FUNCTIONS --------------------------------------------------------
 func _ready() -> void:
 	gen_base()
